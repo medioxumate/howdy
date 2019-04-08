8bba97a1c68cb124d1e42ed563e009a2bcf2c268
[Brian Kiehn] <[briankiehn@comcast.net]>
2019-04-08 18:06:39 -0400

    success

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created in phpStorm.
+ */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Define a deflaut route
+$f3->route('GET /', function () {
+    echo '<h1>Howdy!</h1>';
+});
+
+//Run Fat-free
+$f3->run();
